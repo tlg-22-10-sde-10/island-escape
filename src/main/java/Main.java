7ad6b80d8f70d207f7f2ccf9158aaa81f2e92fc2
Melendez <henry.melendez@tlgcohort.com>
2023-02-02 09:10:29 -0800

    AB#376 finsihed totem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import GameState.GameState;
+import entities.MagicTotem;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
 
     }
 }
