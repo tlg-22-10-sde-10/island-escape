48748810448cbdb3f9bf07982fa455ba9fd36dbb
Kiaya Anderson <kiaya.anderson@tlgcohort.com>
2023-02-07 15:29:05 -0800

    player can now build a boat to escape the island and win, AB#608

---
+++ src/main/java/com/islandescape/controllers/WinConditions.java
@@ -0,0 +1,34 @@
+package com.islandescape.controllers;
+
+import com.islandescape.entities.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class WinConditions {
+
+    public static void playerCanBuildABoat(List<Item> itemList) {
+        Scanner input = new Scanner(System.in);
+        String[] boat = {"paddle", "wood-planks-pile", "rope"};
+        List<String> items = new ArrayList<>();
+
+        for(Item item: itemList){
+            items.add(item.getName());
+        }
+
+        if(items.containsAll(Arrays.asList(boat))){
+            System.out.print("You have obtained all items needed to build a boat and escape.\n Would you like to build a boat now? Yes/No: ");
+            String response = input.nextLine();
+
+            if(response.equalsIgnoreCase("yes")) {
+                System.out.println("The Villagers have sympathy for you and assist you with building the boat.\n" +
+                        "You win!!!! Safe travels home!!!");
+            } else {
+                System.out.println("You have decided to not build a boat at this time. Continue to watch the time. You need to escape!");
+            }
+        }
+    }
+}
+
